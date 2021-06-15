2c6c0f5ba536d2cc33c705a45c73ae594193666c
Jonas <jonas.elst@inuits.eu>
2021-06-15 09:21:37 +0200

    Changed passagiers to inzittenden

--- verzekeringen_api/apps/equipment/enums.py
+++ verzekeringen_api/apps/equipment/enums.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class VehicleType(models.TextChoices):
-    PASSENGER_CAR = "PERSONENWAGEN", "Personenwagen (maximum 5 passagiers)"
-    MINIBUS = "MINIBUS", "Minibus (maximum 8 passagiers)"
-    TRUCK = "VRACHTWAGEN", "Vrachtwagen tot 3.5 ton (maximum 8 passagiers)"
+    PASSENGER_CAR = "PERSONENWAGEN", "Personenwagen (maximum 5 inzittenden)"
+    MINIBUS = "MINIBUS", "Minibus (maximum 8 inzittenden)"
+    TRUCK = "VRACHTWAGEN", "Vrachtwagen tot 3.5 ton (maximum 8 inzittenden)"
 
 
 class VehicleTrailerOption(models.TextChoices):
