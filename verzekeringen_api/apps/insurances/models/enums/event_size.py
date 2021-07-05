7f7d482c60758903bb71e3c08083bbf550c0b672
ricardo <ricardo@inuits.eu>
2021-07-05 13:41:49 +0200

    Ref #79033, small adjustment

--- verzekeringen_api/apps/insurances/models/enums/event_size.py
+++ verzekeringen_api/apps/insurances/models/enums/event_size.py
@@ -4,6 +4,6 @@ from django.db import models
 class EventSize(models.IntegerChoices):
     FIVEHUNDRED = 1, "1-500 (65,55 eur/dag)"
     THOUSAND = 2, "500-1000 (131,10 eur/dag)"
-    THOUSANDFIVEHUNDRED = 3, "1000-1500 (163,88 eur/dag"
+    THOUSANDFIVEHUNDRED = 3, "1000-1500 (163,88 eur/dag)"
     TWOTHOUSAND = 4, "1500-2500 (229,43 eur/dag)"
     TWOTHOUSANDFIVEHUNDRED = 5, "meer dan 2500 (in overleg met Ethias)"
