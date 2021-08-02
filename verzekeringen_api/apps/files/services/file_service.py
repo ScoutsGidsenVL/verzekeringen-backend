8b45ec3765a02e237b1833eebd9f7b469d3ee8f6
Jan Zagata <refuse@inuits.eu>
2021-08-02 11:25:08 +0200

    clean up

--- verzekeringen_api/apps/files/services/file_service.py
+++ verzekeringen_api/apps/files/services/file_service.py
@@ -3,7 +3,6 @@ from ..models import InsuranceClaimAttachment
 
 
 def store_attachment(*, uploaded_file: File, claim: InsuranceClaimAttachment) -> InsuranceClaimAttachment:
-    print(uploaded_file)
     attachment = InsuranceClaimAttachment()
     attachment.insurance_claim = claim
     attachment.file.save(name=uploaded_file.name, content=uploaded_file)
