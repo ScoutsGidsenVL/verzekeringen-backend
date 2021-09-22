0d6f3ac94bbd4cb040136655a8f98c77d8124763
sverholen <sverholen@gmail.com>
2021-09-22 10:21:15 +0200

    Added ssh script from kampvisum

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,47 @@
-#!/bin/sh
+### Base stage
+FROM python:3.7-slim AS base-stage
 
-set -e
+ENV PYTHONFAULTHANDLER=1 \
+    PYTHONHASHSEED=random \
+    PYTHONUNBUFFERED=1 \
+    BASE_DIR=/app
 
-if [ "$1" = "--do-migrate" ]; then
-  echo "Running migrations..."
-  if ! ./manage.py migrate; then
-    exit 1
-  fi
-else
-  echo "Skipping migration..."
-fi
+RUN adduser --no-create-home --disabled-password --home /app --shell /bin/ash app
+WORKDIR /app
 
-echo "Starting development server..."
-exec ./manage.py runserver 0.0.0.0:8000
+COPY docker/entrypoint*.sh /
+COPY verzekeringen_api/manage.py ./
+COPY verzekeringen_api/apps ./apps
+
+ENTRYPOINT ["/entrypoint.sh"]
+
+### Preparation stage
+FROM base-stage AS preparation-stage
+
+ARG APP_ENV
+
+ENV APP_ENV=${APP_ENV} \
+    PIP_DEFAULT_TIMEOUT=100 \
+    PIP_DISABLE_PIP_VERSION_CHECK=1 \
+    PIP_NO_CACHE_DIR=1 \
+    POETRY_VERSION=1.1.4
+
+RUN apt-get update; \
+    apt-get install -y --no-install-recommends \
+        gcc \
+        libpq-dev \
+        libffi-dev \
+        python3-dev \
+        default-libmysqlclient-dev
+
+RUN pip install "poetry==${POETRY_VERSION}"
+
+COPY pyproject.toml poetry.lock* ./
+
+### Development stage
+FROM preparation-stage AS development-stage
+
+RUN poetry config virtualenvs.create false
+RUN poetry install
+
+CMD ["--no-migrate"]
