93d8045589313fba201e910437d663074c33ed25
sverholen <sverholen@gmail.com>
2021-09-22 14:32:13 +0200

    #81237 Add filter for year of accident on insurance claims

---
+++ verzekeringen_api/apps/insurances/api/filters.py
@@ -0,0 +1,26 @@
+import logging
+import uuid
+from django_filters import rest_framework as filters
+from django.db.models import Q
+
+from ..models.insurance_claim import InsuranceClaim
+
+logger = logging.getLogger(__name__)
+
+
+class InsuranceClaimFilter(filters.FilterSet):
+    class Meta:
+        model = InsuranceClaim
+        fields = []
+
+    @property
+    def qs(self):
+        parent = super().qs
+
+        year_of_accident = self.request.query_params("year", None)
+
+        if year_of_accident:
+            logger.debug("Filtering InsuranceClaim instances with year %s", year_of_accident)
+            return parent.filter(date_of_accident__year=year_of_accident)
+
+        return []
