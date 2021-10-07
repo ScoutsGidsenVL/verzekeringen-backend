71e8682cab5e1032d1f0d3514771cf5d55e483c6
sverholen <sverholen@gmail.com>
2021-10-07 14:39:24 +0200

    Bugfix

--- verzekeringen_api/apps/insurances/services/__init__.py
+++ verzekeringen_api/apps/insurances/services/__init__.py
@@ -7,5 +7,5 @@ from .event_insurance_service import EventInsuranceService
 from .equipment_insurance_service import EquipmentInsuranceService
 from .insurance_draft_service import InsuranceDraftService
 from .insurance_claim_mail_service import InsuranceClaimMailService
-from .insurance_claim_service import InsuranceClaimService
 from .insurance_claim_attachment_service import InsuranceClaimAttachmentService
+from .insurance_claim_service import InsuranceClaimService
