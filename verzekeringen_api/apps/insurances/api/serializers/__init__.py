46b864124a32730cdd730e790868066dac1a82a3
sverholen <sverholen@gmail.com>
2021-10-08 09:45:06 +0200

    Proper imports in __init__.py

--- verzekeringen_api/apps/insurances/api/serializers/__init__.py
+++ verzekeringen_api/apps/insurances/api/serializers/__init__.py
@@ -1,10 +1,35 @@
-from .insurance_claim_mixin import *
-from .insurance_type_serializers import *
-from .insurance_serializers import *
-from .insurance_draft_serializers import *
+from .insurance_claim_mixin import InsuranceClaimAdmistrativeFieldsMixin
+from .insurance_type_serializers import InsuranceTypeOutputSerializer
+from .insurance_serializers import (
+    InsuranceCostOutputSerializer,
+    InsuranceListOutputSerializer,
+    BaseInsuranceDetailOutputSerializer,
+    ActivityInsuranceDetailOutputSerializer,
+    TemporaryInsuranceDetailOutputSerializer,
+    TravelAssistanceInsuranceDetailOutputSerializer,
+    TemporaryVehicleInsuranceDetailOutputSerializer,
+    EventInsuranceDetailOutputSerializer,
+    EquipmentInsuranceDetailOutputSerializer,
+    BaseInsuranceCreateInputSerializer,
+    ActivityInsuranceCreateInputSerializer,
+    TemporaryInsuranceCreateInputSerializer,
+    TemporaryVehicleInsuranceCreateInputSerializer,
+    TravelAssistanceInsuranceCreateInputSerializer,
+    EventInsuranceCreateInputSerializer,
+    EquipmentInsuranceCreateInputSerializer,
+)
+from .insurance_draft_serializers import InsuranceDraftOutputSerializer, InsuranceDraftCreateInputSerializer
 from .insurance_claim_attachment_serializers import (
     InsuranceClaimAttachmentUploadSerializer,
     InsuranceClaimAttachmentSerializer,
     FileDetailOutputSerializer,
 )
-from .insurance_claim_serializers import *
+from .insurance_claim_serializers import (
+    InsuranceClaimAttachmentSerializer,
+    InsuranceClaimVictimOutputListSerializer,
+    BaseInsuranceClaimSerializer,
+    InsuranceClaimVictimOutputDetailSerializer,
+    InsuranceClaimVictimInputSerializer,
+    InsuranceClaimDetailOutputSerializer,
+    InsuranceClaimInputSerializer,
+)
