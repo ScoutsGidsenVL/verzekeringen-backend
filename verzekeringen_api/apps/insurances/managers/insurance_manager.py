ad5b9d54695bf9f6a1d2ab01430856c2dc50a9d5
sverholen <sverholen@gmail.com>
2021-11-23 14:41:10 +0100

    Use group_admin_id for responsible member

--- verzekeringen_api/apps/insurances/managers/insurance_manager.py
+++ verzekeringen_api/apps/insurances/managers/insurance_manager.py
@@ -26,7 +26,7 @@ class BaseInsuranceQuerySet(models.QuerySet):
         # Only section leaders can edit and only their own requests
         # Insurance requests can't be edited after they've been approved or invoiced
         return self.filter(
-            _status__in=[InsuranceStatus.NEW, InsuranceStatus.WAITING], responsible_member__ga_id=user.id
+            _status__in=[InsuranceStatus.NEW, InsuranceStatus.WAITING], responsible_member__group_admin_id=user.id
         )
 
 
