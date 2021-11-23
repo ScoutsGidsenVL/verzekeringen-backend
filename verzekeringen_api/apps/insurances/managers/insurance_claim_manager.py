cf0e735b84e7a5bd75244e5bee66d3476e6ed2b7
sverholen <sverholen@gmail.com>
2021-11-23 09:58:03 +0100

    #84242 Sort claims by date of accident

--- verzekeringen_api/apps/insurances/managers/insurance_claim_manager.py
+++ verzekeringen_api/apps/insurances/managers/insurance_claim_manager.py
@@ -1,13 +1,20 @@
+import logging
+
 from django.conf import settings
 from django.db import models
 
 
+logger = logging.getLogger(__name__)
+
+
 class InsuranceClaimQuerySet(models.QuerySet):
     def allowed(self, user: settings.AUTH_USER_MODEL):
+        logger.debug("RETURNING INSURANCE CLAIM QUERYSET allowed")
         user_group_ids = [group.group_admin_id for group in user.scouts_groups]
         return self.filter(group_group_admin_id__in=user_group_ids)
 
 
 class InsuranceClaimManager(models.Manager):
     def get_queryset(self):
-        return InsuranceClaimQuerySet(self.model, using=self._db)
+        logger.debug("Returning INSURANCE CLAIM QUERYSET")
+        return InsuranceClaimQuerySet(self.model, using=self._db).order_by("date_of_accident")
