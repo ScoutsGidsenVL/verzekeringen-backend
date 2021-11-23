b90a9afef20a777e5a453825e382e421f87337fb
sverholen <sverholen@gmail.com>
2021-11-23 11:21:39 +0100

    #79995 Administrators can see all claims

--- verzekeringen_api/apps/insurances/managers/insurance_claim_manager.py
+++ verzekeringen_api/apps/insurances/managers/insurance_claim_manager.py
@@ -9,12 +9,15 @@ logger = logging.getLogger(__name__)
 
 class InsuranceClaimQuerySet(models.QuerySet):
     def allowed(self, user: settings.AUTH_USER_MODEL):
-        logger.debug("RETURNING INSURANCE CLAIM QUERYSET allowed")
+        # Administrators can see all claims
+        if user.has_role_administrator():
+            return self.qs
+
+        # Section and group leaders can see all claims, but only for authorized groups
         user_group_ids = [group.group_admin_id for group in user.scouts_groups]
         return self.filter(group_group_admin_id__in=user_group_ids)
 
 
 class InsuranceClaimManager(models.Manager):
     def get_queryset(self):
-        logger.debug("Returning INSURANCE CLAIM QUERYSET")
         return InsuranceClaimQuerySet(self.model, using=self._db).order_by("date_of_accident")
