6eedd2169c19c6fd39007bda64ea59d42f4b2452
sverholen <sverholen@gmail.com>
2021-12-13 12:52:10 +0100

    Fixed AWS setting problem

--- verzekeringen_api/scouts_auth/inuits/files/aws/s3_storage_service.py
+++ verzekeringen_api/scouts_auth/inuits/files/aws/s3_storage_service.py
@@ -1,39 +1,22 @@
 import logging
 
-from django.conf import settings
 from django.core.files.base import ContentFile
 from django.core.files.storage import FileSystemStorage
 from storages.backends.s3boto3 import S3Boto3Storage
 
-from scouts_auth.inuits.files import StorageService
+from scouts_auth.inuits.files import CustomStorage, StorageSettings
 
 
 logger = logging.getLogger(__name__)
 
 
-class S3StorageService(StorageService, S3Boto3Storage):
+class S3StorageService(CustomStorage, S3Boto3Storage):
 
-    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
-    default_acl = settings.AWS_DEFAULT_ACL
-    file_overwrite = settings.AWS_S3_FILE_OVERWRITE
+    bucket_name = StorageSettings.get_s3_bucket_name()
+    default_acl = StorageSettings.get_s3_default_acl()
+    file_overwrite = StorageSettings.get_s3_file_overwrite()
 
     local_storage = FileSystemStorage()
-
-    # S3Boto3Storage
-    # delete(file_path)
-    # exists(file_path)
-    # listdir(dir_path)
-    # size(file_path)
-    # get_modified_time(file_path)
-
-    # Storage
-    # open(file_path)
-    # save(file_path, file_contents)
-    # path(file_path)
-    # url(file_path)
-
-    def store_file(self, file_path: str, file_contents):
-        return super().save(name=file_path, content=file_contents)
 
     # @TODO remove s3 specific code from this model
     def get_path(self):
@@ -69,11 +52,3 @@ class S3StorageService(StorageService, S3Boto3Storage):
         self.local_storage.save(file_dest_path, ContentFile(remote_file_contents))
 
         return self.local_storage.path(file_dest_path)
-
-    # def delete(self, file_path: str, using=None, keep_parents=False):
-    #     storage = self.file.storage
-
-    #     if storage.exists(self.file.name):
-    #         storage.delete(self.file.name)
-
-    #     super().delete(using=using, keep_parents=keep_parents)
