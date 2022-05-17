8ddf3f4475ee9ae8fb0f90b8460c8aaa11a5eadc
Matthias Dillen <matthias.dillen@inuits.eu>
2022-05-17 13:14:30 +0200

    Refs #88084 make letter_box optional and nullable

--- verzekeringen_api/scouts_auth/inuits/models/inuits_address.py
+++ verzekeringen_api/scouts_auth/inuits/models/inuits_address.py
@@ -5,7 +5,7 @@ from scouts_auth.inuits.models.fields import OptionalCharField, OptionalIntegerF
 class InuitsAddress(AbstractNonModel):
     street = OptionalCharField(max_length=100)
     number = OptionalCharField(max_length=5)
-    letter_box = OptionalCharField(max_length=5)
+    letter_box = OptionalCharField(max_length=5, default=None, blank=True, null=True)
     postal_code = OptionalIntegerField()
     city = OptionalCharField(max_length=40)
     # country = models.ForeignKey(InuitsCountry, on_delete=models.CASCADE, null=True, related_name="address")
@@ -23,6 +23,10 @@ class InuitsAddress(AbstractNonModel):
     #     self.city = kwargs.get("city", "")
 
     def address_details_to_str(self):
-        return "street({}), number({}), letter_box({}), postal_code({}), city({})".format(
-            self.street, self.number, self.letter_box, self.postal_code, self.city
+        if self.letter_box:
+            return "street({}), number({}), letter_box({}), postal_code({}), city({})".format(
+                self.street, self.number, self.letter_box, self.postal_code, self.city
+            )
+        return "street({}), number({}), postal_code({}), city({})".format(
+            self.street, self.number, self.postal_code, self.city
         )
