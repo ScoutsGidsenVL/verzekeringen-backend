f2ff6909955e73f83d9f21cecad5fe3a9dff7c29
Matthias Dillen <matthias.dillen@inuits.eu>
2022-10-26 16:01:02 +0200

    Refs #97516

--- verzekeringen_api/apps/equipment/managers/inuits_vehicle_manager.py
+++ verzekeringen_api/apps/equipment/managers/inuits_vehicle_manager.py
@@ -1,11 +1,17 @@
 from django.conf import settings
 from django.db import models
 
+from scouts_insurances.insurances.models import TemporaryVehicleInsurance
+
 
 class InuitsVehicleQuerySet(models.QuerySet):
     def allowed(self, user: settings.AUTH_USER_MODEL):
         user_group_ids = [group.group_admin_id for group in user.scouts_groups]
-        return self.filter(group_group_admin_id__in=user_group_ids)
+        insurances = TemporaryVehicleInsurance.objects.filter(_group_group_admin_id__in=user_group_ids)
+        allowed_vehicle_ids = list()
+        for insurance in insurances:
+            allowed_vehicle_ids.append(insurance._vehicle_id)
+        return self.filter(id__in=allowed_vehicle_ids)
 
 
 class InuitsVehicleManager(models.Manager):
