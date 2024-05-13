6c744a1aa60ec053daa66b26d48e3d88cd5076a8
Wouter Vanden Hove <wouter@libranet.eu>
2024-05-13 23:05:46 +0200

    black + isort

--- verzekeringen_api/verzekeringen_api/urls.py
+++ verzekeringen_api/verzekeringen_api/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
+
+from django.urls import include, path
 from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 # Open api schema
 schema_view = get_schema_view(
