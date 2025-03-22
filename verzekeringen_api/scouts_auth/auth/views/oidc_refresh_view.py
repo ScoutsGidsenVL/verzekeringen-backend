ad7e3c78a83b672c504432bff304d7835a37053e
Wouter Vanden Hove <wouter@libranet.eu>
2025-03-22 12:16:48 +0100

    add raise from to exception

--- verzekeringen_api/scouts_auth/auth/views/oidc_refresh_view.py
+++ verzekeringen_api/scouts_auth/auth/views/oidc_refresh_view.py
@@ -29,11 +29,14 @@ class OIDCRefreshView(views.APIView):
 
         data = serializer.validated_data
         try:
-            tokens = self.service.get_tokens_by_refresh_token(user=request.user, refresh_token=data.get("refreshToken"))
+            tokens = self.service.get_tokens_by_refresh_token(
+                user=request.user, refresh_token=data.get("refreshToken")
+            )
         except HTTPError as exc:
             logger.error(f"Failed to refresh tokens: {exc}")
             # raise TokenRequestException("Failed to refresh tokens.")
-            raise TokenRequestException(exc)
+            # this custom error takes an exception as input-argument
+            raise TokenRequestException(exc) from exc
 
         output_serializer = TokenSerializer(tokens)
 
