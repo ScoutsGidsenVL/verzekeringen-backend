799262bd323531d7f49dd5f5464822f27a761e22
Yannick Olivier <yannick.olivier@gumbys.be>
2025-04-17 15:35:30 +0200

    T6361 Add caching to /groepsadmin/rest-ga/groep

--- verzekeringen_api/verzekeringen_api/settings.py
+++ verzekeringen_api/verzekeringen_api/settings.py
@@ -226,6 +226,15 @@ DATABASES = {
     }
 }
 
+# Cache
+# https://docs.djangoproject.com/en/5.2/topics/cache/
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "TIMEOUT": 900,
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
