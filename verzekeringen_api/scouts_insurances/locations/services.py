ec3648a7136dc605236b6c102c1fc76e600c8c56
Tinus Van Langendonck <tvl@scoutsengidsenvlaanderen.be>
2026-01-16 12:34:47 +0100

    Gebruik van nieuw groepsadmin endpoint voor alle Belgische gemeentes

--- verzekeringen_api/scouts_insurances/locations/services.py
+++ verzekeringen_api/scouts_insurances/locations/services.py
@@ -12,20 +12,6 @@ class BelgianPostalCodeCityService:
     def _get_dict(self, postal_code: str, city: str) -> dict:
         return {"postal_code": postal_code, "city": city}
 
-    def _search(self, term: str) -> list:
-        payload = {"term": term}
-        response = requests.get(re.sub("^https:http:", "https:", self.endpoint), params=payload)
-
-        response.raise_for_status()
-        json = response.json()
-
-        results = []
-        for record in json:
-            postal_code, city = record.split(" ", 1)
-            results.append({"postal_code": postal_code, "city": city})
-
-        return results
-
     def search(self, term: str) -> list:
         """Search and filter with cached data first, if not found, search the API.
         This method will never cache itself.
@@ -42,13 +28,17 @@ class BelgianPostalCodeCityService:
         return self._search(term)
 
     def _fetch_all(self) -> list:
-        """Searches all data from the API by searching with terms 1-9."""
-        results = []
-        for i in range(1, 10):
-            results.append(self._search(str(i)))
+        response = requests.get(re.sub("^https:http:", "https:", self.endpoint))
 
-        # Flatten array and keep uniques only.
-        return [item for sublist in results for item in sublist]
+        response.raise_for_status()
+        json = response.json()
+
+        results = []
+        for record in json:
+            postal_code, city = record.split(" ", 1)
+            results.append({"postal_code": postal_code, "city": city})
+
+        return results
 
     def fetch_all(self) -> list:
         """Fetch all data and cache it if not already cached."""
